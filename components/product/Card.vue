28d6ca96659463a2076091dced59af99e4643110
Hydra016 <haidermansoor24@gmail.com>
2025-05-14 01:54:00 +0300

    fixed-types

--- components/product/Card.vue
+++ components/product/Card.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import type { Product } from '@/types';
+import type { Product } from '@/types/index';
 
 function defineProps<T>() {
   throw new Error("Function not implemented.");
